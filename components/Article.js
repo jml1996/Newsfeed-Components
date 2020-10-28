0bcf4332000843a70071340e32888ec5f9940f7e
Joshua Lovins <joshualovins@Joshuas-MBP.lan>
2020-10-28 19:58:16 -0400

    Accidentally did Parts 1 and 2 in the main branch. Just checked out a joshua-lovins branch for this commit and push. Still have to do Part 3.

--- components/Article.js
+++ components/Article.js
@@ -84,7 +84,7 @@ const data = [
           hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
 
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
-          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor hodor, HODOR hodor, hoodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   },
   {
